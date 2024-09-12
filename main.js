81765201b6b0d469f0ab14ad553752f88889e6aa
aryan <aryan@mail.com>
2024-09-12 20:14:40 +0530

    Formating

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-
-var typed = new Typed(".text",{
-    strings:["Frontend Developer", "Django Developer", "Web Developer"],
-    typeSpeed : 10,
-    backSpeed : 100,
-    backDelay : 1000,
-    loop : true
+var typed = new Typed(".text", {
+  strings: ["Frontend Developer", "Django Developer", "Web Developer"],
+  typeSpeed: 10,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('submitButton').addEventListener('click', function () {
-                alert('Form submitted!');
-            });
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .getElementById("submitButton")
+    .addEventListener("click", function () {
+      alert("Form submitted!");
+    });
 });
